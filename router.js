c26a628d0abc3a3e240cb36f64be691b019f31e6
justin-aquino <justin.aquino25@gmail.com>
2023-02-17 21:49:00 -0800

    adding router

--- router.js
+++ router.js
@@ -1,5 +1,23 @@
+import { createRouter } from "vue-router";
+import HomePage from "./pages/HomePage.vue";
+
 const routes = [
   {
     path: "/",
+    component: HomePage, //use this component for this path.
+  },
+  {
+    path: "/cash-in",
+    component: CashinPage,
+  },
+  {
+    path: "/send",
+    component: "SendPage",
   },
 ];
+
+//create a router instance
+export default createRouter({
+  history: createWebHistory(), //go to a certain page and you can use the back button to go back
+  routes,
+});
